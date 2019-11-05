755b9020ebe9fe06a6d0c75c7422340f1754afc8
joeycarlos <jclaguio@gmail.com>
2019-11-05 15:41:24 -0800

    First commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public float horizontalOffset = 0f;
+    public float verticalOffset = 0f;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = player.transform.position + new Vector3(horizontalOffset, verticalOffset, -10.0f);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+    }
+}
