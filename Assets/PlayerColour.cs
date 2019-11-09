ac122d1706f205142f50a6cdfacb133e67dee2a5
joeycarlos <jclaguio@gmail.com>
2019-11-09 13:39:15 -0800

    Add rotating player and particles

---
+++ Assets/PlayerColour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColour : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public float timeBetweenColor = 0.2f;
+
+    private void Start() {
+        sr = GetComponent<SpriteRenderer>();
+        StartCoroutine("CycleColor");
+    }
+
+    public void ChangeSprite() {
+        // sr.sprite = newSprite;
+        sr.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    IEnumerator CycleColor() {
+        while (true) {
+            ChangeSprite();
+            yield return new WaitForSeconds(timeBetweenColor);
+        }
+    }
+}
