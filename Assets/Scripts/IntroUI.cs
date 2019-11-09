f7cc0ebad468d017b40cc3d5b35cd39f8b2025ee
joeycarlos <jclaguio@gmail.com>
2019-11-09 14:44:04 -0800

    Add rainbow text to intro and update layers of enemy sprite

--- Assets/Scripts/IntroUI.cs
+++ Assets/Scripts/IntroUI.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class IntroUI : MonoBehaviour
 {
+    public Text gameTitle;
+
+    void Start() {
+        StartCoroutine("CycleColor");
+    }
+
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             SceneManager.LoadScene(1);
+        }
+    }
+
+    public void UpdateColour() {
+        gameTitle.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    IEnumerator CycleColor() {
+        while (true) {
+            UpdateColour();
+            yield return new WaitForSeconds(0.2f);
         }
     }
 }
